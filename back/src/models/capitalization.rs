3f523d9d9ab930f8517f4ed9f3789ce03e082c3a
Serejka <5125402@mail.ru>
2018-08-13 18:14:40 +0300

    added fetcher from coinmarketcap

--- back/src/models/capitalization.rs
+++ back/src/models/capitalization.rs
@@ -15,3 +15,21 @@ pub struct NewCapitalization {
     pub time: SystemTime,
     pub value: f64,
 }
+
+#[derive(Debug, Serialize, Deserialize, Clone, Insertable)]
+#[table_name = "capitalization"]
+pub struct CoinMarketCap {
+    #[serde(rename = "0")]
+    pub time: SystemTime,
+    #[serde(rename = "1")]
+    pub value: f64,
+}
+
+impl From<CoinMarketCap> for NewCapitalization {
+    fn from(cap: CoinMarketCap) -> Self {
+        Self {
+            time: cap.time,
+            value: cap.value,
+        }
+    }
+}
