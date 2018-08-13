3f904056cb396f3ea49e1700121600ebf08ed733
Serejka <5125402@mail.ru>
2018-08-13 10:31:19 +0300

    added git submodule

--- back/src/lib.rs
+++ back/src/lib.rs
@@ -9,6 +9,7 @@ extern crate serde_json;
 #[macro_use]
 extern crate failure;
 extern crate futures;
+extern crate futures_cpupool;
 #[macro_use]
 extern crate log;
 extern crate bigdecimal;
@@ -16,6 +17,8 @@ extern crate bigdecimal;
 extern crate diesel;
 #[macro_use]
 extern crate diesel_derive;
+extern crate r2d2;
+extern crate r2d2_diesel;
 
 mod client;
 pub mod config;
@@ -25,13 +28,53 @@ mod models;
 mod schema;
 mod types;
 
+
+use r2d2_diesel::ConnectionManager;
+use diesel::pg::PgConnection;
+use futures_cpupool::CpuPool;
+
 use self::config::Config;
 use self::environment::Environment;
 use failure::Fail;
 use futures::Future;
 
+
+use hyper::{Body, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::{self, Future};
+
+
 pub fn current_block_number(config: Config) {
     let env = Environment::new(config);
+
+    // Prepare server
+    let thread_count = config.server.thread_count;
+
+    // Prepare server
+    let address = {
+        format!("{}:{}", config.server.host, config.server.port)
+            .parse()
+            .expect("Could not parse address")
+    };
+
+    // Prepare database pool
+    let database_url: String = config.server.database.parse().expect("Database URL must be set in configuration");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let db_pool = r2d2::Pool::builder().build(manager).expect("Failed to create connection pool");
+
+
+    let new_service = || {
+        service_fn_ok(|_| {
+            Response::new(Body::from(PHRASE))
+        })
+    };
+
+    let server = Server::bind(&address)
+        .serve(new_service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    rt::run(server);
+
     let future = env
         .ethereum_client
         .fetch_current_block_number()
